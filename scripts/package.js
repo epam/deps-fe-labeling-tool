ff35a5c4802defc249b1c0aa708e34b932124e22
Artsiom_Murashko <Artsiom_Murashko@epam.com>
2023-09-12 16:04:17 +0300

    [15557] Refactor configs to publish LT

--- scripts/package.js
+++ scripts/package.js
@@ -1,27 +1,10 @@
 const exec = require('child_process').exec
 const fs = require('fs')
-const fse = require('fs-extra')
 
-fs.rmSync('package', { recursive: true, force: true })
+fs.rmSync('lib', { recursive: true, force: true })
 
-exec('mkdir package && cd package && mkdir lib && babel --config-file ../package.babel.js ../src -d lib --extensions .js,.jsx,.snap --copy-files --no-copy-ignored', (err) => {
+exec('mkdir lib && babel --config-file ./package.babel.js ./src -d lib --extensions .js,.jsx,.snap --copy-files --no-copy-ignored', (err) => {
   if (err) {
     throw err
-  } else {
-    fse.copy('dist', 'package/dist', (err) => {
-      if (err) {
-        throw err
-      }
-    })
-    fs.copyFile('package.json', 'package/package.json', (err) => {
-      if (err) {
-        throw err
-      }
-    })
-    fs.copyFile('yarn.lock', 'package/yarn.lock', (err) => {
-      if (err) {
-        throw err
-      }
-    })
   }
 })
