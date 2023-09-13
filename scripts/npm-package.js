b48d9bcec0feeb0d61ab8bcf3208d49b077b6e54
Artsiom_Murashko <Artsiom_Murashko@epam.com>
2023-09-13 12:33:04 +0300

    [15557] Fix comments

--- scripts/npm-package.js
+++ scripts/npm-package.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 fs.rmSync('lib', { recursive: true, force: true })
 
-exec('mkdir lib && babel --config-file ./package.babel.js ./src -d lib --extensions .js,.jsx,.snap --copy-files --no-copy-ignored', (err) => {
+exec('mkdir lib && babel --config-file ./npm-package.babel.js ./src -d lib --extensions .js,.jsx,.snap --copy-files --no-copy-ignored', (err) => {
   if (err) {
     throw err
   }
